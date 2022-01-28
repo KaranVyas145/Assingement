76e77c57a1d8ee93327a7308a5574c0c4ab62222
Karan Vyas <karanvyas2907@gmail.com>
2022-01-28 22:28:22 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+// table with id age-table 
+let table = document.getElementById("age-table");
+
+// label elements inside the table 
+let label = table.getElementsByTagName("label");
+
+// first td in the table with word age 
+table.getElementsByTagName('td')[0];
+
+// form with name search 
+let search = document.getElementsByName('search')[0];
+
+// first input 
+let input = document.querySelector("input");
+
+// last input 
+let lastInput= document.querySelectorAll("input");
+lastInput[lastInput.length-1]
